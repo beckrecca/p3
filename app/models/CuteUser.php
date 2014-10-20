dfc936d509e7f57158de3ab4b0b094a4e934433c
Rebecca Doris <rebecca.doris42@gmail.com>
2014-10-20 13:17:00 -0400

    added comments to methods

--- app/models/CuteUser.php
+++ app/models/CuteUser.php
@@ -2,18 +2,24 @@
 
 Class CuteUser {
   /** Properties **/
-  private $name; // String
-  private $dob; // String
-  private $loc; // String
+  private $name; // String of the user's name
+  private $dob; // String of the user's date of birth
+  private $loc; // String of the user's location
 
   /** Methods **/
 
+  /**
+  * Sets up the new user with a name, DOB, and location.
+  **/
   public function setUser() {
     $this->setName();
     $this->setDOB();
     $this->setLoc();
   }
-
+  
+  /**
+  * Sets the user's name.
+  **/
   public function setName() {
     $this->name = $this->randomName();
   }
@@ -25,23 +31,37 @@ Class CuteUser {
     return $this->name;
   }
 
+  /**
+  * Sets the user's date of birth.
+  **/
   public function setDOB() {
     $this->dob = $this->randomDOB();
   }
 
+  /**
+  * Simple getter method for the DOB variable.
+  **/
   public function getDOB() {
     return $this->dob;
   }
 
+  /**
+  * Sets the user's location.
+  **/
   public function setLoc() {
     $this->loc = $this->RandomLocation();
   }
 
+  /**
+  * Simple getter method for the user's location.
+  **/
   public function getLoc() {
     return $this->loc;
   }
 
-
+  /**
+  * Generates a random silly name for the user.
+  **/
   private function randomName() {
     // Create an array of names
     $names = array("Kissy Von Smoocherface", "Cupcakes McGee", "Tom Bombadil",
@@ -60,7 +80,10 @@ Class CuteUser {
                       "Gunther", "Eureka Nutt");
     return $names[rand(0, count($names)-1)];
   }
-
+  
+  /**
+  * Generates a random DOB for the user.
+  **/
   private function randomDOB() {
     $month = rand(1,12);
     $year = rand(1900,2000);
@@ -70,7 +93,10 @@ Class CuteUser {
 
   }
 
-  private function randomDayOfTheMonth($month, $year) {
+  /**
+  * Returns a random day of the month, depending on the length of the month.
+  **/
+  private function randomDayOfTheMonth($month) {
     $day;
     if ($month == 2) {
        $day = rand(0,28);
@@ -82,6 +108,9 @@ Class CuteUser {
     return $day;
   }
 
+  /**
+  * Generates a random silly location for the user.
+  **/
   private function randomLocation() {
     $locations = array("Seattle", "Walla Walla", "Candy Kingdom", "Teribithia", "Middle-Earth",
                         "San Francisco", "Inside of a giant peach pit", "Boston", "London", 
